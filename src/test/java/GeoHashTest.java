c724795ea02fd9b21d37abee4e5ae06adae8fcae
gboztepe <gozdeboztepe@gmail.com>
2020-08-24 09:43:55 +0300

    created student branch

--- src/test/java/GeoHashTest.java
+++ src/test/java/GeoHashTest.java
@@ -17,57 +17,5 @@ public class GeoHashTest {
 
     }
 
-    @Test
-    @Owner("gboztepe")
-    @Severity(SeverityLevel.CRITICAL)
-    @Issue("GEOHASH-2")
-    @Story("Verilen geohash degerinden lat ve lon degerleri elde edilmelidir.")
-    @Description("Test decode function")
-    public void decode() {
-        Coordinates coordinates = GeoHash.decode("u120fxw");
-        Coordinates coordinatesActual = new Coordinates(52.205,0.119);
-        Assert.assertEquals(coordinates.getLat(), coordinatesActual.getLat(), 0.001);
-        Assert.assertEquals(coordinates.getLon(), coordinatesActual.getLon(),0.001);
-    }
 
-    @Test
-    @Owner("gboztepe")
-    @Severity(SeverityLevel.NORMAL)
-    @Issue("GEOHASH-3")
-    @Story("Verilen geohash degerinden bounding box lat ve lon degerleri elde edilmelidir.")
-    @Description("Test bounds function")
-    public void bounds() {
-        Bounds bounds = GeoHash.bounds("u120fxw");
-        Assert.assertEquals(52.205, bounds.getNe().getLat(), 0.001);
-        Assert.assertEquals(0.119, bounds.getNe().getLon(), 0.001);
-
-        Assert.assertEquals(52.204, bounds.getSw().getLat(), 0.001);
-        Assert.assertEquals(0.118, bounds.getSw().getLon(), 0.001);
-
-    }
-
-    @Test
-    @Owner("gboztepe")
-    @Severity(SeverityLevel.NORMAL)
-    @Issue("GEOHASH-4")
-    @Story("Verilen geohash ve yon bilgisinden komsulugunda bulunan alanin geohash degeri elde edilmelidir.")
-    @Description("Test adjacent function")
-    public void adjacent() {
-        String northN = GeoHash.adjacent("u120fxw", "N");
-        String actualNorthN = GeoHash.encode(52.2063, 0.1188, 7);
-        Assert.assertEquals(northN, actualNorthN);
-
-    }
-
-    @Test
-    @Owner("gboztepe")
-    @Severity(SeverityLevel.MINOR)
-    @Issue("GEOHASH-5")
-    @Story("Verilen geohash ve yon bilgisinden komsulugunda bulunan alanin geohash degeri elde edilmelidir.")
-    @Description("Test neighbours function")
-    public void neighbours() {
-        String northN = GeoHash.neighbours("u120fxw", "ne");
-        String actualNorthN = GeoHash.encode(52.2063, 0.1202, 7);
-        Assert.assertEquals(northN, actualNorthN);
-    }
 }
